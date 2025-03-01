375fce116d12b306f75200fff95dd6ce7e816813
Francisco Alcala <alcalafrancisco@gmail.com>
2025-02-28 19:58:02 -0500

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,6 @@
 ﻿(function () {
     const OPENAI_API_KEY = process.env.OPENAI_API_KEY2;  // Replace with your actual OpenAI API key
+    console.log(OPENAI_API_KEY); 
     const ASSISTANT_ID = "asst_GoUA2iVIVKi2xUCEvf9Lrjko";  // Virtual Weiter assistant ID
     const CHATBOT_NAME = "Virtual Waiter";
     const AVATAR_URL = "https://vecoin.github.io/jshosting/logo_v3_blickbot.jpg";
